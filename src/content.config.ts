ee222b51e946042e85113693a60f7f1eea5cee15
Thiago <thiago6891@gmail.com>
2026-06-17 16:57:50 -0300

    Fix deprecated import

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 
 const caseStudies = defineCollection({
 	loader: glob({ pattern: '**/*.md', base: './src/content/case-studies' }),
